67876a2065dc5fa97f5e97956efb24de252c85e5
GregoryE <GregoryE@GGSP006470>
2023-10-02 22:32:18 +0800

    Add project files.

---
+++ WPFListviewAlternatingRowColors/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPFListviewAlternatingRowColors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Person> _PersonCollection = new ObservableCollection<Person>();
+
+        public MainWindow()
+        {
+            PersonCollection.Add(new Person() { Name = "test1", Address = "Guam", Contact = "test1@mail.com" });
+            PersonCollection.Add(new Person() { Name = "test2", Address = "Guam", Contact = "test1@mail.com" });
+            PersonCollection.Add(new Person() { Name = "test3", Address = "Hongkong", Contact = "test1@mail.com" });
+            PersonCollection.Add(new Person() { Name = "test4", Address = "Guam", Contact = "test1@mail.com" });
+            PersonCollection.Add(new Person() { Name = "test5", Address = "Moscow", Contact = "test1@mail.com" });
+
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+
+        public ObservableCollection<Person> PersonCollection
+        {
+            get
+            {
+                return _PersonCollection;
+            }
+        }
+    }
+}
---
+++ WPFListviewAlternatingRowColors/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace WPFListviewAlternatingRowColors
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+
+        public Person()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            Contact = string.Empty;
+        }
+    }
+}
